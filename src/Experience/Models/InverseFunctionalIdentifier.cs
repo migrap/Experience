f217f1fe6d5df46932769d978807dd7f1259b8e1
Michael Park <migrap@gmail.com>
2014-06-23 16:26:57 -0700

    Uncommitted changes

--- src/Experience/Models/InverseFunctionalIdentifier.cs
+++ src/Experience/Models/InverseFunctionalIdentifier.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Experience.Models {
-	//Inverse Functional Identifier
-	public class InverseFunctionalIdentifier {        
+    //Inverse Functional Identifier
+    public class InverseFunctionalIdentifier {
         public Mailto Mbox { get; set; }
-		public string Shasum { get; set; }
-		public string Openid { get; set; }
-		public Account Account { get; set; }
-	}
-}
+        public string Shasum {
+            get { return Mbox.GetHash(); }
+        }
+        public string Openid { get; set; }
+        public Account Account { get; set; }
+    }
 
+    public static partial class Extensions {
+        private static SHA512CryptoServiceProvider _provider = new SHA512CryptoServiceProvider();
+
+        public static string GetHash(this Mailto self) {
+            if(null == self) {
+                return string.Empty;
+            }
+
+            var mailto = self.ToString();
+            var buffer = Encoding.UTF8.GetBytes(mailto);
+            var hash = _provider.ComputeHash(buffer);
+            hash = _provider.ComputeHash(hash);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
